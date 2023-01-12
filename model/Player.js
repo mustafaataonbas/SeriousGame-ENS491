49cd07b10eb8979ba7b3de9bec9a65a36c98d383
Mustafa Ata Onbas <aonbas@sabanciuniv.edu>
2023-01-12 23:46:49 +0300

    Dev - MongoDB added + signup

---
+++ model/Player.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const playerSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'please add a name']
+    },
+    surname: {
+        type: String,
+        required: [true, 'please add a name']
+    },
+    username: {
+        type: String,
+        required: [true, 'please add a name'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'please add a password'],
+        
+    },    
+    lastAuthentication: Date,
+},
+{
+    timestamps: true,
+})
+
+module.exports = mongoose.model('Players', playerSchema)
